a50a70f3bbf5f9fd000cdd933fdb4eed30e630f2
yss1235-why <cofclanone1@gmail.com>
2025-09-14 20:06:47 +0530

    Create useTheme.ts

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { restaurantConfig } from '../config/restaurant';
+
+export const useTheme = () => {
+  useEffect(() => {
+    // Remove any existing theme links
+    const existingThemeLinks = document.querySelectorAll('link[data-theme]');
+    existingThemeLinks.forEach(link => link.remove());
+
+    // Create and append new theme CSS link
+    const themeLink = document.createElement('link');
+    themeLink.rel = 'stylesheet';
+    themeLink.href = `/src/themes/${restaurantConfig.theme_name}.css`;
+    themeLink.setAttribute('data-theme', restaurantConfig.theme_name);
+    
+    document.head.appendChild(themeLink);
+  }, []);
+
+  return {
+    restaurantName: restaurantConfig.res_name,
+    themeName: restaurantConfig.theme_name
+  };
+};
