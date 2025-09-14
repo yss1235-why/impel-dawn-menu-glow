e05f929160c7d3610a7c609897b5f800e49c9725
yss1235-why <cofclanone1@gmail.com>
2025-09-14 20:08:41 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,27 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { restaurantConfig } from "./config/restaurant";
+
+// Update document title with restaurant name
+document.title = `${restaurantConfig.res_name} - Premium Restaurant Menu`;
+
+// Dynamically load theme CSS
+const loadTheme = () => {
+  // Remove any existing theme links
+  const existingThemeLinks = document.querySelectorAll('link[data-theme]');
+  existingThemeLinks.forEach(link => link.remove());
+
+  // Create and append new theme CSS link
+  const themeLink = document.createElement('link');
+  themeLink.rel = 'stylesheet';
+  themeLink.href = `/src/themes/${restaurantConfig.theme_name}.css`;
+  themeLink.setAttribute('data-theme', restaurantConfig.theme_name);
+  
+  document.head.appendChild(themeLink);
+};
+
+// Load theme before rendering
+loadTheme();
 
 createRoot(document.getElementById("root")!).render(<App />);
