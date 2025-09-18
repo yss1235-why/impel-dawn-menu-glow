9d91886181c08ace67617f0cd374565710fdd3fb
yss1235-why <cofclanone1@gmail.com>
2025-09-18 18:07:36 +0530

    Update restaurant.ts

--- src/config/restaurant.ts
+++ src/config/restaurant.ts
@@ -1,4 +1,5 @@
 export const restaurantConfig = {
   res_name: "Your's Cafe",
-  theme_name: "red" // Options: "gold", "blue", "purple", "red"
+  theme_name: "red", // Options: "gold", "blue", "purple", "red"
+  menu_data: "yours-cafe" // Options: "impel-dawn", "yours-cafe"
 };
